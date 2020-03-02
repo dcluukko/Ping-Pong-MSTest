5d9762949c2e49f2c67981b3d017f8134e1a777f
Student <example@example.com>
2020-03-02 15:54:52 -0800

    completed tests and refactored code

--- Replace.Tests/ModelTests/PingPongTests.cs
+++ Replace.Tests/ModelTests/PingPongTests.cs
@@ -14,5 +14,29 @@ namespace PingPong.Tests
 			PingPongCount testPingPong = new PingPongCount();
 			Assert.AreEqual(false, testPingPong.ToPingPong("K"));
 		}
+		[TestMethod]
+		public void ToPingPong_ReturnRangeFromInput_True()
+		{
+			PingPongCount testPingPong = new PingPongCount();
+			Assert.AreEqual(true, testPingPong.ToPingPong("6"));
+		}
+		[TestMethod]
+		public void ToPingPong_ReplaceNumbersDivisibleByThree_True()
+		{
+			PingPongCount testPingPong = new PingPongCount();
+			Assert.AreEqual("Ping", testPingPong.ToPingPong("6"));
+		}
+		[TestMethod]
+		public void ToPingPong_ReplaceNumbersDivisibleByFive_True()
+		{
+			PingPongCount testPingPong = new PingPongCount();
+			Assert.AreEqual("Pong", testPingPong.ToPingPong("10"));
+		}
+		[TestMethod]
+		public void ToPingPong_ReplaceNumbersDivisibleByThreeAndFive_True()
+		{
+			PingPongCount testPingPong = new PingPongCount();
+			Assert.AreEqual("Ping-Pong", testPingPong.ToPingPong("15"));
+		}
 	}
 }
--- Replace/Models/PingPong.cs
+++ Replace/Models/PingPong.cs
@@ -1,14 +1,87 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace PingPong
 {
 	public class PingPongCount
 	{
-		public bool ToPingPong(string input)
+
+		public List<string> ToPingPong(string input)
 		{
-			int number;
-			return TryParse(input, out number);
+		int inputCount = int.Parse(input);
+		List<string> Output = new List<string>();
+		for (int i = 1; i<= inputCount; i++)
+			{
+				if (i % 15 == 0)
+				{
+					Output.Add("Pong");
+				}
+				else if (i % 3 == 0)
+				{
+					Output.Add("Ping");
+				}
+				else if (i % 5 == 0 )
+				{
+					Output.Add("Ping-Pong");
+				}
+				else
+				{
+					string num = i.ToString();
+					Output.Add(num);
+				}
+			}			
+		return Output;
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+// namespace PingPong
+// {
+// 	public class PingPongCount
+// 	{
+// 		public static string ToPingPong(string input)
+// 		{
+
+// 			if (int.TryParse(input, out int number)) 
+// 			{
+// 				int inputCount = int.Parse(input);
+// 				for (int i = 	1; i <= inputCount; i++)
+// 				{
+// 					if (i % 15 == 0)
+// 					{
+// 						Console.WriteLine("Ping-Pong");
+// 						return "Ping-Pong";
+// 					}
+// 					else if (i % 3 == 0)
+// 					{
+// 						Console.WriteLine("Ping");
+// 						return "Ping";
+// 					}
+// 					else if (i % 5 == 0 )
+// 					{
+// 						Console.WriteLine("Pong");
+// 						return "Pong";
+// 					}
+// 					else
+// 					{
+// 					Console.WriteLine(i);
+// 					}
+// 				}
+// 			} else 
+// 			{
+// 				Console.WriteLine("Hey! That's not a number!");
+// 			}
+// 			return input.ToString();
+// 		}
+// 	}
+// }
